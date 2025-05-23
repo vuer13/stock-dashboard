2a8010a5b79692ede60e246dbb5731dcffa9a90f
Ryan Vu <ryanvu1131@gmail.com>
2025-05-23 01:23:36 -0700

    main

--- core/main.py
+++ core/main.py
@@ -3,13 +3,13 @@ from scipy.optimize import brentq
 import numpy as np
 
 def bs_price(S, K, T, r, sigma, option_type = 'call'):
-    d1 = (np.log(S/K) + (r + 0.5 * sigma**2) * t)/(sigma/np.sqrt(T))
-    d2 = d1 - sigma * np.sqrt(t)
+    d1 = (np.log(S/K) + (r + 0.5 * sigma**2) * T)/(sigma/np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
 
     if option_type == 'call':
-        return S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
+        return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
     else:
-        return K * np.exp(-r * t) * norm.cdf(d2) - S * norm.cdf(d1)
+        return K * np.exp(-r * T) * norm.cdf(d2) - S * norm.cdf(d1)
 
 def implied_volatility(S, K, T, r, option_price, option_type = 'call'):
     try:
